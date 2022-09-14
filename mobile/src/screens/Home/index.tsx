a900d9f2f202e1ccdeae1764cd0ee60449a65767
CleitonCandiotto <cleitoncandiotto@gmail.com>
2022-09-13 23:22:42 -0300

    Trabalhando no Mobile

---
+++ mobile/src/screens/Home/index.tsx
@@ -0,0 +1,31 @@
+import { View, Image } from "react-native";
+
+import { styles } from "./style";
+
+import logoImg from "../../assets/logo-nlw-esports.png"
+
+import { Heading } from "../../components/Heading";
+
+import { GAMES } from "../../utils/games";
+
+import { GameCard } from "../../components/GameCard";
+
+export function Home(){
+    return (
+        <View style={styles.container}>
+            <Image
+                source={logoImg}
+                style={styles.logo}
+            />
+
+            <Heading
+                title="Encontre seu duo!"
+                subtitle="Selecione o game que deseja jogar..."
+            />
+
+            <GameCard
+                data={GAMES[0]}
+            />
+        </View>
+    )
+}
